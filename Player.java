29209dfda583cdbece1b583681c6c4140bc0b024
ubbiout <ubbiout@gmail.com>
2018-01-12 22:47:47 -0700

    Intentionally broken
    unintentionally

--- Player.java
+++ Player.java
@@ -21,14 +21,9 @@ public class Player {
 
         //M. trying not to break our server lol
         try {//M. just for testing purposes, the following code isn't smart and really only works on default map as blue
-            //M. let's do some rocket research!
-            if (gc.round() == 1) {
-                //System.out.println("Current round: " + gc.round() + " let's research a rocket!");
-                gc.queueResearch(UnitType.Rocket); //M. research some rocket!
-            }
-
-            while (gc.round() == 1) { //M. why can't this be an if statement?
-                //System.out.println("Current round: " + gc.round());
+            
+            while (gc.round()%2 == 0) { //M. why can't this be an if statement?
+                System.out.println("Current round: " + gc.round() + " let's go West");
                 // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
                 VecUnit units = gc.myUnits();
                 for (int i = 0; i < units.size(); i++) {
@@ -41,68 +36,24 @@ public class Player {
                 }
                 gc.nextTurn(); //M. this is all we will do on turn 1
             }
-            while (gc.round() >= 2 && gc.round() <= 8) { //M. let's gc.replicate(int worker_id, Direction direction)
-                //System.out.println("Current round: " + gc.round() + " let's replicate to the west");
+            while (gc.round()%2 == 1) { //M. why can't this be an if statement?
+                System.out.println("Current round: " + gc.round() + " let's go East");
                 // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
                 VecUnit units = gc.myUnits();
                 for (int i = 0; i < units.size(); i++) {
                     Unit unit = units.get(i);
-                    if (gc.canReplicate(unit.id(), Direction.West)) { //M. canReplicate: Whether the worker is ready to replicate. Tests that the worker's ability heat is sufficiently low, that the team has sufficient karbonite in its resource pool, and that the square in the given direction is empty.
-                        //System.out.println("Unit replicating: " + unit.id()); //M.
-                        try {
-                            //System.out.println("Replican(t)!");
-                            gc.replicate(unit.id(), Direction.West);
-                        } catch (Exception e) { //M. end of try()
-                            //System.err.println("Exception caught: " + e.getMessage()); //gerald here
-                            e.printStackTrace();
-                        }//M. end of catch()
+
+                    // Most methods on gc take unit IDs, instead of the unit objects themselves.
+                    if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.East)) {
+                        gc.moveRobot(unit.id(), Direction.East);
                     }
                 }
-                gc.nextTurn(); //M. this is all we will do on turn 1-5
+                gc.nextTurn(); //M. this is all we will do on turn 1
             }
 
-            //build a factory on round 9+
-            while (gc.round() > 8 && gc.round() < 45) { //M. just build factories forever lol... on the same square
-                //System.out.println("Current round: " + gc.round() + " let's build a factory with " + gc.karbonite() + "karbonite in the bank");
-                VecUnit units = gc.myUnits();
-                for (int i = 0; i < units.size(); i++) { //M. all the workers/units will try to do the same thing
-                    Unit unit = units.get(i);
-                    if (gc.karbonite() >= (300 / 4)) { //M. 300/4 is blueprint cost of factory. factory max hp/4
-                        //System.out.println("Unit constructing Blueprint: " + unit.id());
-                        //if (gc.canBlueprint(unit.id(), UnitType.Worker, Direction.South)) { //M. this fucking stops us
-                            //System.out.println("Unit: " + unit.id() + " attempts to construct");
-                            try {
-                                //System.out.println("Blueprint exists!");
-                                gc.blueprint(unit.id(), UnitType.Factory, Direction.South);
-                            } catch (Exception e) { //M. end of try()
-                                //System.err.println("Exception caught: " + e.getMessage()); //gerald here
-                                //e.printStackTrace();
-                            }//M. end of catch()
-                        //}
-                    }
-                    if(unit.maxHealth() == 300) { //300 is factory maxHealth... so it's a factory?
-                        //System.out.println("Unit " + unit.id() + "is a factory!");
-
-                        //if(gc.canBuild(unit.id(), need blueprint_id here)){ //M.idk what a blueprint_id is
-
-                        //}
-                    }
-                    if(unit.maxHealth() == 100) { //300 is worker and healer maxHealth... so it's a worker?
-                        //System.out.println("Unit " + unit.id() + "is a worker!");
-                        //Location site = unit.location();
-                        //Unit aFactory = gc.senseUnitAtLocation(); //senseUnitAtLocation(MapLocation location)
-
-                        //if(gc.canBuild(unit.id(), need blueprint_id here)){ //M.idk what a blueprint_id is
-
-                        //}
-                    }
-                }
-                // Submit the actions we've done, and wait for our next turn.
-                gc.nextTurn(); //M. if we get to this point... end our turn
-            }
-            //System.out.println("Current round: " + gc.round() + " I got past everything!");
+            System.out.println("Current round: " + gc.round() + " I got past everything!");
             // Submit the actions we've done, and wait for our next turn.
-            gc.nextTurn(); //M. if we get to this point... end our turn. I don't know why this stops everything, but i don't want to crash server LOL OH GAWD
+            //gc.nextTurn(); //M. if we get to this point... end our turn. I don't know why this stops everything, but i don't want to crash server LOL OH GAWD
 
         } catch (Exception e) { //M. end of try()
             //System.err.println("Exception caught: " + e.getMessage()); //gerald here
